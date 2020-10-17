336a70b3f400269da00c834ef52d3f6af35abad8
Steven Bister <stevenbister@macbook-pro.home>
2020-10-17 17:25:31 +0100

    Added buttons to add and remove extra players

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Player from './components/Player';
 
-function App() {
+const App = () => {
+  const [ players, setPlayers ] = useState([<Player />]);
+
+  // This let's us add multiple players to the app
+  const addPlayer = () => {
+    setPlayers([
+      ...players,
+      <Player />
+    ])
+  }
+
+  // Aaaaaand this removes the latst one added
+  const removePlayer = () => {
+    if ( players.length > 1 ) {
+      players.pop();
+
+      setPlayers([
+        ...players,
+      ])
+    }
+  }
+
   return (
     <div className="App">
-      <Player />
+      <button onClick={addPlayer}>Add player</button>
+      <button onClick={removePlayer}>Remove player</button>
+
+      { players.map( player => player ) }
     </div>
   );
 }
