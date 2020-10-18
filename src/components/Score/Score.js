af3d89d39871e7b6929e82ac0ad49d6216a0eb20
Steven Bister <stevenbister@macbook-pro.home>
2020-10-18 15:42:05 +0100

    Added proptypes
    Also adusted the props transformations so it's easier to read

--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { stringToArray, arrayValuesToNumbers, sumArray } from '../../utils/arrayManipulators'
 import './Score.css';
 
 const Score = ({score}) => {
-  const totalScore = sumArray(arrayValuesToNumbers(stringToArray(score)))
+  const scoreArr = stringToArray(score);
+  const totalScore = sumArray(arrayValuesToNumbers(scoreArr));
+
   return (
     <footer className='Score'>
       Score: { isNaN(totalScore) ? 0 : totalScore }
     </footer>
   )
+}
+
+Score.propTypes = {
+  score: PropTypes.string,
 }
 
 export default Score;
