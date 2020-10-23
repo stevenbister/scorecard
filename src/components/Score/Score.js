098d31653ffbcad93ea9abb2e682cf189aa89c8c
Steven Bister <stevenbister@macbook-pro.home>
2020-10-23 12:49:23 +0100

    As score is being calucated hight up we no longer need the functions here

--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { stringToArray, arrayValuesToNumbers, sumArray } from '../../utils/arrayManipulators'
 import './Score.css';
 
-const Score = ({score}) => {
-  const scoreArr = stringToArray(score);
-  const totalScore = sumArray(arrayValuesToNumbers(scoreArr));
-
-  return (
-    <footer className='Score'>
-      Score: { isNaN(totalScore) ? 0 : totalScore }
-    </footer>
-  )
-}
+const Score = ({score}) => (
+  <footer className='Score'>
+    Score: {score}
+  </footer>
+)
 
 Score.propTypes = {
   score: PropTypes.string,
