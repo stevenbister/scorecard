ab1d13b21bca9d3861c56c430d1f6d72af898b25
Steven Bister <stevebister@gmail.com>
2020-11-18 11:37:59 +0000

    Added toggle colour state function

--- src/components/ColorSwitcher/ColorSwitcher.js
+++ src/components/ColorSwitcher/ColorSwitcher.js
@@ -5,8 +5,12 @@ import { FiSun, FiMoon } from 'react-icons/fi'
 const ColorSwitcher = () => {
   const [ color, setColor ] = useState('light');
 
+  const toggleColorState = () => color === 'light' ? setColor('dark') : setColor('light');
+
   return (
-    <button aria-label="Light mode" ><FiSun /></button>
+    <button aria-label="Light mode" onClick={ toggleColorState }>
+      { color === 'light' ? <FiSun /> : <FiMoon /> }
+    </button>
   );
 }
 
