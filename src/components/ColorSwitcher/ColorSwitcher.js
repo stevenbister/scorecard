23205df881f0f01cd47f022265aa09fe9d967219
Steven Bister <stevebister@gmail.com>
2020-11-18 11:43:29 +0000

    Updated to implicit return

--- src/components/ColorSwitcher/ColorSwitcher.js
+++ src/components/ColorSwitcher/ColorSwitcher.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { FiSun, FiMoon } from 'react-icons/fi'
 
-const ColorSwitcher = ({ color, handleClick }) => {
-
-  return (
-    <button aria-label="Light mode" onClick={ handleClick }>
-      { color === 'light' ? <FiSun /> : <FiMoon /> }
-    </button>
-  );
-}
+const ColorSwitcher = ({ color, handleClick }) => (
+  <button aria-label="Light mode" onClick={ handleClick }>
+    { color === 'light' ? <FiSun /> : <FiMoon /> }
+  </button>
+)
 
 export default ColorSwitcher;
