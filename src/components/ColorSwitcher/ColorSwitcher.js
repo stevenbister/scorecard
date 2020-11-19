124bf383b21b2cff0f8e44c88aeafa6dc4b2f0c2
Steven Bister <stevebister@gmail.com>
2020-11-19 11:57:44 +0000

    Brought state controls down from parent component & removed props
    This will make the component more reusable in the long run

--- src/components/ColorSwitcher/ColorSwitcher.js
+++ src/components/ColorSwitcher/ColorSwitcher.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useMediaQuery } from 'react-responsive';
 import { FiSun, FiMoon } from 'react-icons/fi'
 import './ColorSwitcher.css'
 
-const ColorSwitcher = ({ color, handleClick }) => (
-  <button className='App__button Switcher' aria-label={color === 'light' ? 'Activate dark mode' : 'Activate light mode'} onClick={ handleClick }>
-    { color === 'light' ? <FiSun /> : <FiMoon /> }
-  </button>
-)
+const ColorSwitcher = () => {
+
+  // Check if the prefered colour scheme is light or dark and set the state accordingly
+  const systemPrefersDark = useMediaQuery({
+    query: '(prefers-color-scheme: dark)' },
+    undefined,
+    (prefersDark) => setisDark(prefersDark)
+  );
+
+  const [ isDark, setisDark ] = useState(systemPrefersDark);
+
+  // Toggle the state of the colour
+  const toggleColorState = () =>  isDark ? setisDark(false) : setisDark(true);
+
+  return (
+    <button className='App__button Switcher' aria-label={isDark ? 'Activate light mode' : 'Activate dark mode'} onClick={ toggleColorState }>
+      { isDark ? <FiMoon /> : <FiSun /> }
+    </button>
+  );
+}
 
 ColorSwitcher.propTypes = {
   color: PropTypes.string,
