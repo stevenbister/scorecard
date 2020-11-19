f8056344a8543564883581639b96c9934db14c10
Steven Bister <stevebister@gmail.com>
2020-11-19 11:50:52 +0000

    Added react-responsive to handle changing the media query
    Removed useeffect from component
    
    Updated state to handle boolean rather than strings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, {useState, useEffect} from 'react';
-import { AiOutlineUserAdd, AiOutlineUserDelete } from 'react-icons/ai'
+import React, { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { AiOutlineUserAdd, AiOutlineUserDelete } from 'react-icons/ai';
 
 import Player from './components/Player/Player';
-import ColorSwitcher from './components/ColorSwitcher/ColorSwitcher'
+import ColorSwitcher from './components/ColorSwitcher/ColorSwitcher';
 import './App.css';
 
 const App = () => {
   const [ players, setPlayers ] = useState([<Player />]);
-  const [ color, setColor ] = useState('light');
 
   // This let's us add multiple players to the app
   const addPlayer = () => {
@@ -29,26 +29,25 @@ const App = () => {
   }
 
   // Check if the prefered colour scheme is light or dark and set the state accordingly
-  useEffect( () => {
-    const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+  // TODO: move this back out into the component, can handle adding the class with something like useeffect
+  const systemPrefersDark = useMediaQuery({
+    query: '(prefers-color-scheme: dark)' },
+    undefined,
+    (prefersDark) => setisDark(prefersDark)
+  );
 
-    isDarkMode.matches ? setColor('dark') : setColor('light');
-
-    isDarkMode.addEventListener('change', () => {
-      color === 'light' ? setColor('dark') : setColor('light');
-    })
-  }, [color] )
+  const [ isDark, setisDark ] = useState(systemPrefersDark);
 
   // Toggle the state of the colour
-  const toggleColorState = () => color === 'light' ? setColor('dark') : setColor('light');
+  const toggleColorState = () =>  isDark ? setisDark(false) : setisDark(true);
 
   return (
-    <div className={`App ${ color === 'dark' ? 'App--dark' : '' }`}>
+    <div className={`App ${ isDark ? 'App--dark' : '' }`}>
       <header className='App__header'>
         <h1>Scorecard</h1>
 
         <div className='App__button--container'>
-          <ColorSwitcher color={color} handleClick={ toggleColorState } />
+          <ColorSwitcher color={isDark} handleClick={ toggleColorState } />
 
           <button className='App__button App__button--primary' onClick={addPlayer} aria-label='Add player'><AiOutlineUserAdd /></button>
           <button className='App__button App__button--secondary' onClick={removePlayer} aria-label='Remove player'><AiOutlineUserDelete /></button>
