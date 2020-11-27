c1432f253c210add1e38ecb6f8a3612d633222f3
Steven Bister <stevebister@gmail.com>
2020-11-27 11:33:07 +0000

    Added check for NaN's and output nicer value
    Would be better to only allow number values as a user input!

--- src/utils/arrayManipulators.js
+++ src/utils/arrayManipulators.js
@@ -29,8 +29,14 @@ const arrayValuesToNumbers = (arr) => arr.map( value => Number(value))
 // Get the sum of the array
 const sumArray = (arr) =>  {
   if (Array.isArray(arr) && arr.length) {
-    return arr.reduce((accumulator, currentValue) => accumulator + currentValue);
+    const result = arr.reduce((accumulator, currentValue) => accumulator + currentValue);
+
+    // Catch NaN value and return 0 as a nice output for the user
+    if (isNaN(result)) return 0;
+
+    return result;
   }
+
   return 0;
 }
 
