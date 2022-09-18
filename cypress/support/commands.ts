ea58e997103f8ea72c353c6325d21fc0ed013aea
Steve Bister <stevebister@gmail.com>
2022-09-18 20:12:27 +0100

    feat: adds custom login cypress function

---
+++ cypress/support/commands.ts
@@ -0,0 +1,62 @@
+export {};
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Logs in with a random user. Yields the user and adds an alias to the user
+       *
+       * @returns {typeof login}
+       * @memberof Chainable
+       * @example
+       *    cy.login()
+       * @example
+       *    cy.login({ email: 'whatever@example.com' })
+       */
+      login: typeof login;
+
+      /**
+       * Extends the standard visit command to wait for the page to load
+       *
+       * @returns {typeof visitAndCheck}
+       * @memberof Chainable
+       * @example
+       *    cy.visitAndCheck('/')
+       *  @example
+       *    cy.visitAndCheck('/', 500)
+       */
+      visitAndCheck: typeof visitAndCheck;
+    }
+  }
+}
+
+function login() {
+  Cypress.log({
+    name: "Supabase login",
+  });
+  // programmatically log us in without needing the UI
+  cy.request({
+    method: "POST",
+    url: "/login",
+    form: true,
+    body: {
+      password: "test1234!",
+      email: "test@example.com",
+    },
+  });
+
+  cy.getCookie("__session").should("exist");
+}
+
+// We're waiting a second because of this issue happen randomly
+// https://github.com/cypress-io/cypress/issues/7306
+// Also added custom types to avoid getting detached
+// https://github.com/cypress-io/cypress/issues/7306#issuecomment-1152752612
+// ===========================================================
+function visitAndCheck(url: string, waitTime: number = 1000) {
+  cy.visit(url);
+  cy.location("pathname").should("contain", url).wait(waitTime);
+}
+
+Cypress.Commands.add("login", login);
+Cypress.Commands.add("visitAndCheck", visitAndCheck);
