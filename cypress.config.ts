f3a34eb5b04bb836fdc1d1412c04561d42fdfeeb
Steve Bister <stevebister@gmail.com>
2022-09-18 17:05:03 +0100

    feat: sets up cypress configs

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,30 @@
 import { defineConfig } from "cypress";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+
+dotenv.config();
 
 export default defineConfig({
-  projectId: "fwkmco",
   e2e: {
-    baseUrl: "http://localhost:3000"
+    setupNodeEvents: (on, config) => {
+      const isDev = config.watchForFileChanges;
+      const port = process.env.PORT ?? (isDev ? "3000" : "8811");
+      const configOverrides: Partial<Cypress.PluginConfigOptions> = {
+        baseUrl: `http://localhost:${port}`,
+        video: !process.env.CI,
+        screenshotOnRunFailure: !process.env.CI,
+      };
+
+      // To use this:
+      // cy.task('log', whateverYouWantInTheTerminal)
+      on("task", {
+        log: (message) => {
+          console.log(message);
+
+          return null;
+        },
+      });
+
+      return { ...config, ...configOverrides };
+    },
   },
 });
