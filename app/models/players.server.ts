6234da03b79e4d544b68bd5b138b355365668c86
Steven Bister <stevebister@gmail.com>
2022-10-02 13:42:59 +0100

    refactor: abstract client into own file

--- app/models/players.server.ts
+++ app/models/players.server.ts
@@ -1,28 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
-import invariant from "tiny-invariant";
+import { supabase } from "./supabaseClient.server";
 import type { definitions } from "~/types/supabase";
 
 export type User = { id: string; email: string };
-
-// Abstract this away
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
-const supabaseServiceRole = process.env.SUPABASE_SERVICE_ROLE; // Node only DO NOT EXPOSE TO FRONTEND
-
-invariant(
-  supabaseUrl,
-  "SUPABASE_URL must be set in your environment variables."
-);
-invariant(
-  supabaseAnonKey,
-  "SUPABASE_ANON_KEY must be set in your environment variables."
-);
-invariant(
-  supabaseServiceRole,
-  "SUPABASE_SERVICE_ROLE must be set in your environment variables."
-);
-
-export const supabase = createClient(supabaseUrl, supabaseServiceRole);
 
 export async function addNewPlayer({ id, name }: { id: string; name: string }) {
   const { data, error } = await supabase
