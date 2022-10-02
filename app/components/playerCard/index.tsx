d3aafbbb658d9d726b83aad133241bd1524ca6e2
Steven Bister <stevebister@gmail.com>
2022-10-02 10:41:27 +0100

    feat: adds user to players list

--- app/components/playerCard/index.tsx
+++ app/components/playerCard/index.tsx
@@ -1,41 +1,55 @@
 import { Avatar, Button, Stack, Text } from "@chakra-ui/react";
 import { useFetcher } from "@remix-run/react";
 import type { definitions } from "~/types/supabase";
+import { useUser } from "~/utils";
 import Table from "../table";
 
 interface Props {
   player: definitions["players"] & {
+    name?: definitions["profiles"]["name"];
     game_stats: definitions["game_stats"][];
   };
 }
 
 export default function PlayerCard({ player }: Props) {
   const fetcher = useFetcher();
-  const { player_name, game_stats } = player;
+  const user = useUser();
+  const { name, player_name, game_stats } = player;
+
+  const isCurrentUser = user.id === player.id ? true : false;
 
   return (
     <Stack direction="row" alignItems="center" spacing={1}>
       <Stack spacing={1}>
-        <Avatar name={player_name} size="lg" />
-        <Text textAlign="center">{player_name}</Text>
+        <Avatar name={player_name ? player_name : name} size="lg" />
+        <Text textAlign="center">{player_name ? player_name : name}</Text>
       </Stack>
 
       <Stack>
         <Table stats={game_stats[0]} />
 
-        <fetcher.Form method="post" noValidate>
-          <input
-            type="hidden"
-            id="player_id"
-            name="player_id"
-            value={player.id}
-          />
-          <input type="hidden" id="delete" name="_action" value="DELETE" />
+        {/* Only show our delete option if the player isn't the current user */}
+        {!isCurrentUser ? (
+          <fetcher.Form method="post" noValidate>
+            <input
+              type="hidden"
+              id="player_id"
+              name="player_id"
+              value={player.id}
+            />
+            <input type="hidden" id="delete" name="_action" value="DELETE" />
 
-          <Button size="sm" type="submit">
-            Delete
-          </Button>
-        </fetcher.Form>
+            <Button
+              size="sm"
+              type="submit"
+              isLoading={
+                fetcher.state === "submitting" || fetcher.state === "loading"
+              }
+            >
+              Delete
+            </Button>
+          </fetcher.Form>
+        ) : null}
       </Stack>
     </Stack>
   );
