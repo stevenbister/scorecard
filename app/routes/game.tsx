fa1d1de338ff322353484a333cda75e465a9c57e
Steven Bister <stevebister@gmail.com>
2022-10-02 16:35:09 +0100

    feat: displays current game

--- app/routes/game.tsx
+++ app/routes/game.tsx
@@ -3,7 +3,13 @@ import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
-import { addNewGame } from "~/models/games.server";
+import PlayerDrawer from "~/components/playerDrawer";
+import {
+  addNewGame,
+  addPlayersToGame,
+  getCurrentGame,
+} from "~/models/games.server";
+import { getAllPlayersByUserId } from "~/models/players.server";
 import { createGameSession, getGameId, getUser } from "~/session.server";
 
 type LoaderData = {};
@@ -15,35 +21,57 @@ export const loader: LoaderFunction = async ({ request }) => {
   if (!user) return redirect("/login");
   if (!gameId) return redirect("/");
 
+  console.log(gameId);
+
+  const currentGame = await getCurrentGame(gameId);
+
+  const players = await getAllPlayersByUserId(user.id);
+  if (!players) return;
+
   return json<LoaderData>({
-    user,
-    gameId,
+    players,
+    currentGame,
   });
 };
 
 export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const _action = formData.get("_action");
+  const players = formData.getAll("player");
+
   const user = await getUser(request);
   if (!user) return redirect("/login");
 
-  const game = await addNewGame(user.id);
+  if (_action === "CREATE_GAME") {
+    const game = await addNewGame(user.id);
 
-  if (!game)
-    throw new Response("Something went wrong creating the game!", {
-      status: 500,
+    if (!game)
+      throw new Response("Something went wrong creating the game!", {
+        status: 500,
+      });
+
+    return createGameSession({
+      request,
+      gameId: game.id,
+      redirectTo: "/game",
     });
+  }
 
-  return createGameSession({
-    request,
-    gameId: game.id,
-    redirectTo: "/game",
-  });
+  if (_action === "ADD_PLAYER") {
+    const gameId = await getGameId(request);
+
+    await addPlayersToGame(gameId, { user_id: user.id, players: [...players] });
+
+    return null;
+  }
 };
 
 export default function Game() {
-  const loader = useLoaderData();
+  const { players, currentGame } = useLoaderData();
   return (
     <>
-      <pre>{JSON.stringify(loader, null, 4)}</pre>
+      <pre>{JSON.stringify(currentGame, null, 4)}</pre>
+      <PlayerDrawer players={players} playersInGame={currentGame.players} />
 
       <Form action="/game/end-game" method="post">
         <Button type="submit">End Game</Button>
