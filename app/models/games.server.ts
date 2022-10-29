bd9f0879ebbbe2525f10c534a69b910278a43915
Steven Bister <stevebister@gmail.com>
2022-10-29 10:23:57 +0100

    feat: add function to get all player ids with an active game state

--- app/models/games.server.ts
+++ app/models/games.server.ts
@@ -1,5 +1,7 @@
 import type { definitions } from "~/types/supabase";
+import { getPlayerById } from "./players.server";
 import { supabase } from "./supabaseClient.server";
+import { getProfileById } from "./user.server";
 
 export async function addNewGame(user_id: string) {
   const { data, error } = await supabase
@@ -80,4 +82,46 @@ export async function addPlayersToGame(
     throw new Response(error.message, { status: 500 });
   }
   if (data) return data;
+}
+
+export async function getPlayersInGame(id: string) {
+  const { data, error } = await supabase
+    .from<definitions["games"]>("games")
+    .select("players")
+    .eq("id", id)
+    .single();
+
+  if (error) {
+    console.error(error);
+  }
+
+  const playersInGame: object[] = [];
+
+  await Promise.all(
+    // @ts-ignore -- wtf?
+    data?.players?.map(async (playerId: string) => {
+      try {
+        const u = await getProfileById(playerId); // user/profiles table
+        const p = await getPlayerById(playerId); // players table
+
+        if (u) {
+          // @ts-ignore -- want to dynamically add the score object and that doesn't exist in our db
+          u.score = 0;
+          playersInGame.push(u);
+        }
+
+        if (p) {
+          // @ts-ignore -- want to dynamically add the score object and that doesn't exist in our db
+          p.score = 0;
+          playersInGame.push(p);
+        }
+
+        return [p, u];
+      } catch (error) {
+        throw error;
+      }
+    })
+  );
+
+  return playersInGame;
 }
