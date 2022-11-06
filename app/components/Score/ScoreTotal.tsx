10a7ee16d19778083837755cb3cc751f284a5dd7
Steven Bister <stevebister@gmail.com>
2022-11-06 10:31:11 +0000

    refactor: remove unused prop

--- app/components/Score/ScoreTotal.tsx
+++ app/components/Score/ScoreTotal.tsx
@@ -1,11 +1,9 @@
 import { Text } from "@chakra-ui/react";
-import type { definitions } from "~/types/supabase";
 
 interface Props {
-  activePlayer: definitions["players"]["id"] | definitions["profiles"]["id"];
   score: number;
 }
 
-export default function Score({ activePlayer, score }: Props) {
-  return <Text data-active-player={activePlayer}>Score: {score}</Text>;
+export default function Score({ score }: Props) {
+  return <Text>Score: {score}</Text>;
 }
