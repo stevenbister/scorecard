08ea2c4afb0460a50fbbfa6b12c611d9433d7af8
Steven Bister <stevebister@gmail.com>
2024-03-19 18:50:18 +0000

    feat: add header component

---
+++ src/components/Header/Header.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+<header>
+    <h1>Scorecard</h1>
+</header>
+
+<style>
+    header {
+        display: flex;
+        padding: var(--size-fluid-2);
+        border-bottom: 1px solid var(--accent-color);
+    }
+
+    h1 {
+        font-size: var(--font-size-fluid-3);
+    }
+</style>
