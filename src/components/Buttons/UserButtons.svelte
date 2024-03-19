1e21969ba535b4647d22b733e8463ac3955233a4
Steven Bister <stevebister@gmail.com>
2024-03-19 19:37:11 +0000

    feat: adds user button group

---
+++ src/components/Buttons/UserButtons.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+    import UserMinus from "../Icons/UserMinus.svelte";
+    import UserPlus from "../Icons/UserPlus.svelte";
+    import VisuallyHidden from "../VisuallyHidden/VisuallyHidden.svelte";
+
+    type UserButton = {
+        icon: typeof UserMinus | typeof UserPlus;
+        label: string;
+        class: string;
+    }
+
+    type UserButtons = Record<string, UserButton>;
+
+    const userButtons: UserButtons = {
+        add: {
+            icon: UserPlus,
+            label: "Add user",
+            class: "btn--add"
+        },
+        remove: {
+            icon: UserMinus,
+            label: "Remove user",
+            class: "btn--remove"
+        }
+    }
+</script>
+
+<div>
+    {#each Object.keys(userButtons) as button}
+        <button class={`btn ${userButtons[button].class}`}>
+            <svelte:component this={userButtons[button].icon} />
+            <VisuallyHidden>{userButtons[button].label}</VisuallyHidden>
+        </button>
+    {/each}
+</div>
+
+<style>
+    div {
+        display: flex;
+    }
+
+    .btn {
+        background-color: transparent;
+    }
+
+    .btn.btn--add {
+        color: var(--green-9)
+    }
+
+    .btn.btn--remove {
+        color: var(--red-8)
+    }
+
+    @media (prefers-color-scheme: dark) {
+        .btn.btn--add {
+            color: var(--green-3)
+        }
+        .btn.btn--remove {
+            color: var(--red-3)
+        }
+    }
+</style>
--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -1,15 +1,19 @@
 <script>
-
+    import UserButtons from "../Buttons/UserButtons.svelte";
 </script>
+
 <header>
     <h1>Scorecard</h1>
+
+    <UserButtons />
 </header>
 
 <style>
     header {
         display: flex;
+        justify-content: space-between;
         padding: var(--size-fluid-2);
-        border-bottom: 1px solid var(--accent-color);
+        border-bottom: var(--border-size-1) solid var(--accent-color);
     }
 
     h1 {
