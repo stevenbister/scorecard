76d8af1836ffa2c2537fcb14bcbfc045ffceb3d6
Steven Bister <stevebister@gmail.com>
2024-03-20 20:26:51 +0000

    feat: add player name input

---
+++ src/components/Game/PlayerNameInput.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import store from '../../stores/players';
+    import { debounce } from '../../utils';
+
+    export let playerId: number | undefined = undefined;
+    export let name: string | undefined = undefined;
+
+    function handleInput() {
+        if (playerId === undefined || playerId === null) {
+            throw new Error('playerId must be defined');
+        }
+
+        if (name === undefined) {
+            name = '';
+        }
+
+        store.updatePlayerName(playerId, name);
+    }
+</script>
+
+<input
+    type="text"
+    bind:value={name}
+    on:input={debounce(() => handleInput(), 200)}
+/>
+
+<style>
+    input {
+        width: 100%;
+        font-size: var(--font-size-3);
+        font-weight: var(--font-weight-7);
+        padding-inline: var(--container-padding);
+    }
+</style>
