ae9c9f39e1cd542cf93a309876968a25ff57a6e0
Steven Bister <stevebister@gmail.com>
2024-03-22 11:48:09 +0000

    feat: add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svelte()],
+    plugins: [
+        svelte(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                // Toggle this to true for testing in dev mode
+                enabled: false,
+            },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+            },
+            includeAssets: ['favicon.ico', 'Icon.png', 'Icon512.png'],
+            manifest: {
+                name: 'Scorecard',
+                short_name: 'Scorecard',
+                description: 'My Awesome Scorecard',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'favicon.ico',
+                        sizes: '64x64 32x32 24x24 16x16',
+                        type: 'image/x-icon',
+                    },
+                    {
+                        src: 'Icon.png',
+                        type: 'image/png',
+                        sizes: '128x128',
+                    },
+                    {
+                        src: 'Icon512.png',
+                        type: 'image/png',
+                        sizes: '512x512',
+                    },
+                ],
+            },
+        }),
+    ],
 });
